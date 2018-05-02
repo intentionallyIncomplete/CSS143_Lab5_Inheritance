002504203919c21194857ffdea4273dd934ef6c6
Ian Bryan <incomplete@users.noreply.github.com>
2018-05-02 15:08:26 -0700

    Equals Method
    Equals method in both SimpleColor and ColorWithAlpha overridden and boolean logic is now in place.

--- src/ColorWithAlpha.java
+++ src/ColorWithAlpha.java
@@ -5,9 +5,9 @@
 /****************************************************/
 
 public class ColorWithAlpha extends SimpleColor{
-	
+
 	int alpha;
-	
+
 	/****************************************************/
 	/*						Getters						*/
 	/****************************************************/
@@ -15,7 +15,7 @@ public class ColorWithAlpha extends SimpleColor{
 	public int getAlpha(){
 		return alpha;
 	}
-	
+
 	/****************************************************/
 	/* 						Setters						*/
 	/****************************************************/
@@ -26,7 +26,7 @@ public class ColorWithAlpha extends SimpleColor{
 			this.alpha = alpha;
 		}
 	}
-	
+
 	/****************************************************/
 	/*						Ctors			 			*/
 	/****************************************************/
@@ -36,7 +36,7 @@ public class ColorWithAlpha extends SimpleColor{
 		setG(0);
 		setB(0);
 	}
-	
+
 	//this ctor will give the values of red, green, and blue
 	//to the ctor of the SimpleColor class that takes 3 args. 
 	//the ctor of the SimpleColor class will then do the assignment
@@ -46,12 +46,13 @@ public class ColorWithAlpha extends SimpleColor{
 		super(r,g,b);
 		setAlpha(alpha);
 	}
-	
+
 	//the copy ctor
 	public ColorWithAlpha(ColorWithAlpha aCopyOf){
-		this(aCopyOf.alpha);
+		super(aCopyOf);
+		this.alpha = aCopyOf.alpha;
 	}
-	
+
 	/****************************************************/
 	/*				Custom override methods				*/
 	/* first overridden method is the toString method	*/
@@ -60,11 +61,17 @@ public class ColorWithAlpha extends SimpleColor{
 	/* this will compare the values of the current RGBA	*/
 	/* to those passed in to the class.					*/
 	/****************************************************/
-	
+
 	public String toString(){
 		return super.toString() + ", alpha: " + alpha;
 	}
-	
+
+	//this method overrides the default equals method. 
+	//The overridden equals method from the super class (SimpleColor)
+	//is used to compare the objects using RBG and then the
+	//value of the alpha from this class is checked against
+	//the new object's alpha. If both the object in RBG values,
+	//and the alpha value from this class are the same, they must be equal.
 	public boolean equals(ColorWithAlpha other){
 		if(super.equals(other) == true && this.alpha == other.alpha){
 			return true;
